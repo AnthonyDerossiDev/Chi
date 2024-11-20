e17d71407cc17416a6dddcfeb19fe47080fc82a7
AnthonyLeites <anthonyderossicode@gmail.com>
2024-11-20 19:03:41 +0100

    Altar de monedas por objetos

---
+++ Source/RogueLike/CustomTextRenderComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/TextRenderComponent.h"
+#include "CustomTextRenderComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class ROGUELIKE_API UCustomTextRenderComponent : public UTextRenderComponent
+{
+	GENERATED_BODY()
+	
+};
