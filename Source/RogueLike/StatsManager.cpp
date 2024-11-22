20dda2107864a936a23489c1243d8a92f857f14d
AnthonyLeites <anthonyderossicode@gmail.com>
2024-11-23 00:21:00 +0100

    Stun pick up

--- Source/RogueLike/StatsManager.cpp
+++ Source/RogueLike/StatsManager.cpp
@@ -88,8 +88,9 @@ void UStatsManager::SetValuesOnBegin()
 void UStatsManager::SetDefaultValues()
 {
 	// Asignaciónn de aquellos valores que SÍ O SÍ han de estar a un mínimo, de lo contrario el resultado será 0
-	StatsMap[EPlayerStatType::CriticalDamage].BaseValue = 2.0f;
-	StatsMap[EPlayerStatType::ProjectileSpeed].BaseValue = 1.0f;
+	if(StatsMap[EPlayerStatType::CriticalDamage].BaseValue == 0) StatsMap[EPlayerStatType::CriticalDamage].BaseValue = 2.0f;
+	if(StatsMap[EPlayerStatType::ProjectileSpeed].BaseValue == 0) StatsMap[EPlayerStatType::ProjectileSpeed].BaseValue = 1.0f;
+	if(StatsMap[EPlayerStatType::StunDuration].BaseValue == 0) StatsMap[EPlayerStatType::StunDuration].BaseValue = 1.0f;
 }
 
 
