1de620b7866661ea8fb3c27c45396428ad09509a
AnthonyDerossiDev <77489535+AnthonyDerossiDev@users.noreply.github.com>
2025-02-12 19:24:07 +0100

    WIP Characters Stats

---
+++ Source/RogueLike/Private/BaseCharacterStats.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseCharacterStats.h"
+
+BaseCharacterStats::BaseCharacterStats()
+{
+}
+
+BaseCharacterStats::~BaseCharacterStats()
+{
+}
