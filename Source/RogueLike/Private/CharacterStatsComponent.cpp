81901ae3a19d87ab5922370eb92bfdae56c3453a
AnthonyDerossiDev <77489535+AnthonyDerossiDev@users.noreply.github.com>
2025-03-19 01:45:23 +0100

    EXP/Gold System and rewards

--- Source/RogueLike/Private/CharacterStatsComponent.cpp
+++ Source/RogueLike/Private/CharacterStatsComponent.cpp
@@ -208,4 +208,33 @@ float UCharacterStatsComponent::GetFinalStatWithSkill(ECharacterStatType Stat, f
 	return FinalStatValue;
 }
 
+void UCharacterStatsComponent::AddPlayerExperience(const float Amount)
+{
+	CurrentExperience += Amount;
+
+	if(CurrentExperience >= NeededExperience)
+	{
+		LevelUp();
+	}
+}
+
+void UCharacterStatsComponent::LevelUp()
+{
+	const float DifferenceExperience = CurrentExperience - NeededExperience;
+	CurrentExperience = DifferenceExperience;
+	CurrentPlayerLevel++;
+	NeededExperience += NeededExperienceIncrementPerLevel;
+
+	IncrementStatsByLevel();
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Player has leveled up!"));
+}
+
+void UCharacterStatsComponent::IncrementStatsByLevel()
+{
+	for (auto& Pair : CurrentPlayerStatsMap)
+	{
+		Pair.Value.Value += Pair.Value.LevelIncrement;
+	}
+}
+
 
